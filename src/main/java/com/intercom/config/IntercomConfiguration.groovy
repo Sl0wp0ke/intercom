f1f5e7b452403b841955c1b311791fb612df7b02
Davyd_Rybak <Davyd_Rybak@epam.com>
2017-01-04 17:08:32 +0200

    Mongodb property are added

---
+++ src/main/java/com/intercom/config/IntercomConfiguration.groovy
@@ -0,0 +1,10 @@
+package com.intercom.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@ComponentScan("com.intercom")
+@Configuration
+class IntercomConfiguration {
+    
+}
